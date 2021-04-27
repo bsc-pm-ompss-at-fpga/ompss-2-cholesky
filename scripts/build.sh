a7f227c0dc1e3af2ac5fb87357aa73fa948bb63a
Jaume <jbosch@bsc.es>
2021-04-27 12:34:31 +0200

    Add CI scripts

---
+++ scripts/build.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -e
+
+if [ "$BOARD" == "" ]; then
+  echo "BOARD environment variable not defined"
+  exit 1
+elif [ "$FPGA_HWRUNTIME" == "" ]; then
+  echo "FPGA_HWRUNTIME environment variable not defined"
+  exit 1
+fi
+
+OUT_DIR=$(pwd -P)/build
+RES_FILE=$(pwd -P)/resources_results.json
+
+if [ "$NODE_NAME" == "hca-ikergune" ]; then
+  module load openblas/0.3.6_arm64
+elif [ "$NODE_NAME" == "llebeig" ]; then
+  module load openblas/0.3.13/sequential
+elif [ "$NODE_NAME" == "xaloc" ]; then
+  module load openblas/0.3.10/sequential
+else
+  echo "NODE_NAME ('$NODE_NAME') not supported. Cannot load OpenBLAS."
+  exit 1
+fi
+
+mkdir -p $OUT_DIR
+
+if [ "$1" == "binary" ]; then
+  #Only build the binaries
+  make clean
+  make cholesky-p cholesky-d
+  mv cholesky-p cholesky-d $OUT_DIR
+else
+  make clean
+  make bitstream-p LDFLAGS=--Wf,--disable_utilization_check
+  mv cholesky-p $OUT_DIR
+  mv cholesky_ait/cholesky.bin $OUT_DIR/bitstream.bin
+  mv cholesky_ait/cholesky.bit $OUT_DIR/bitstream.bit
+
+  printf "{\"version\": \"${FPGA_HWRUNTIME}_${BLOCK_SIZE}\", " >>$RES_FILE
+  printf "\"hwruntime\": \"${FPGA_HWRUNTIME}\", " >>$RES_FILE
+  printf "\"accels_freq\": \"${FPGA_CLOCK}\", " >>$RES_FILE
+  printf "\"memory_port_width\": \"${FPGA_MEMORY_PORT_WIDTH}\", " >>$RES_FILE
+  printf "\"benchmark\": \"cholesky\", " >>$RES_FILE
+  printf "\"BRAM\": \"" >>$RES_FILE
+  grep "BRAM" cholesky_ait/cholesky.resources-hls.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"DSP\": \"" >>$RES_FILE
+  grep "DSP" cholesky_ait/cholesky.resources-hls.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"FF\": \"" >>$RES_FILE
+  grep "FF" cholesky_ait/cholesky.resources-hls.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"LUT\": \"" >>$RES_FILE
+  grep "LUT" cholesky_ait/cholesky.resources-hls.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"BRAM_IMPL\": \"" >>$RES_FILE
+  grep "BRAM" cholesky_ait/cholesky.resources-impl.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"DSP_IMPL\": \"" >>$RES_FILE
+  grep "DSP" cholesky_ait/cholesky.resources-impl.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"FF_IMPL\": \"" >>$RES_FILE
+  grep "FF" cholesky_ait/cholesky.resources-impl.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"LUT_IMPL\": \"" >>$RES_FILE
+  grep "LUT" cholesky_ait/cholesky.resources-impl.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"WNS\": \"" >>$RES_FILE
+  grep "WNS" cholesky_ait/cholesky.timing-impl.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"TNS\": \"" >>$RES_FILE
+  grep "TNS" cholesky_ait/cholesky.timing-impl.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"NUM_ENDPOINTS\": \"" >>$RES_FILE
+  grep "NUM_ENDPOINTS" cholesky_ait/cholesky.timing-impl.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\", \"NUM_FAIL_ENDPOINTS\": \"" >>$RES_FILE
+  grep "NUM_FAIL_ENDPOINTS" cholesky_ait/cholesky.timing-impl.txt | awk '{printf $2}' >>$RES_FILE
+  printf "\"},\n" >>$RES_FILE
+fi
---
+++ scripts/exec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -el
+
+RES_FILE=$(pwd -P)/test_results.json
+
+for EXEC_MODE in d p; do
+  for MATRIX_SIZE in 2048 4096; do
+    echo "=== Check mode: ${EXEC_MODE}, msize: ${MATRIX_SIZE} ==="
+    ##NOTE: Check == 2 -> enables the warm-up mode
+    CHECK=$([ "$MATRIX_SIZE" == "2048" ] && echo 1 || echo 2)
+    NX_ARGS="--summary" timeout --preserve-status 150s ./build/cholesky-${EXEC_MODE} ${MATRIX_SIZE} ${CHECK}
+    cat test_result.json >>$RES_FILE
+    echo "," >>$RES_FILE
+  done
+done
