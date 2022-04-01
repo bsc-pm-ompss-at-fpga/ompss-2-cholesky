47ea8e77c43f0adb9a84e10ee204d729598994e3
Miquel Vidal Piñol <miquel.vidal@bsc.es>
2022-04-01 17:49:34 +0200

    Added placement files

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -7,7 +7,7 @@ for EXEC_MODE in d p; do
     echo "=== Check mode: ${EXEC_MODE}, msize: ${MATRIX_SIZE} ==="
     ##NOTE: Check == 2 -> enables the warm-up mode
     CHECK=$([ "$MATRIX_SIZE" == "5120" ] && echo 1 || echo 2)
-    NX_ARGS="--summary --fpga-alloc-pool-size=4G" timeout --preserve-status 150s ./build/cholesky-${EXEC_MODE} ${MATRIX_SIZE} ${CHECK}
+	NX_ARGS="--summary --smp-workers=12 --fpga-alloc-pool-size=4G" timeout --preserve-status 150s ./build/cholesky-${EXEC_MODE} ${MATRIX_SIZE} ${CHECK}
     cat test_result.json >>$RES_FILE
     echo "," >>$RES_FILE
   done
