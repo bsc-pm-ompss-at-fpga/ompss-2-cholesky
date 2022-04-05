1e60532ea8e54f9218a294d92ab37eb942e76d02
Miquel Vidal Piñol <miquel.vidal@bsc.es>
2022-04-05 12:33:06 +0200

    ZCU102 specific run configuration

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -2,13 +2,26 @@
 
 RES_FILE=$(pwd -P)/test_results.json
 
-for EXEC_MODE in d p; do
-  for MATRIX_SIZE in 5120 10240; do
-    echo "=== Check mode: ${EXEC_MODE}, msize: ${MATRIX_SIZE} ==="
-    ##NOTE: Check == 2 -> enables the warm-up mode
-    CHECK=$([ "$MATRIX_SIZE" == "5120" ] && echo 1 || echo 2)
-	NX_ARGS="--summary --smp-workers=12 --fpga-alloc-pool-size=4G" timeout --preserve-status 150s ./build/cholesky-${EXEC_MODE} ${MATRIX_SIZE} ${CHECK}
-    cat test_result.json >>$RES_FILE
-    echo "," >>$RES_FILE
-  done
-done
+if [ "$BOARD" == "alveo_u200" ]; then
+	for EXEC_MODE in d p; do
+	  for MATRIX_SIZE in 5120 10240; do
+	    echo "=== Check mode: ${EXEC_MODE}, msize: ${MATRIX_SIZE} ==="
+	    ##NOTE: Check == 2 -> enables the warm-up mode
+	    CHECK=$([ "$MATRIX_SIZE" == "5120" ] && echo 1 || echo 2)
+		NX_ARGS="--summary --smp-workers=12 --fpga-alloc-pool-size=4G" timeout --preserve-status 150s ./build/cholesky-${EXEC_MODE} ${MATRIX_SIZE} ${CHECK}
+	    cat test_result.json >>$RES_FILE
+	    echo "," >>$RES_FILE
+	  done
+	done
+elif [ "$BOARD" == "zcu102" ]; then
+	for EXEC_MODE in d p; do
+	  for MATRIX_SIZE in 2048 4096; do
+	    echo "=== Check mode: ${EXEC_MODE}, msize: ${MATRIX_SIZE} ==="
+	    ##NOTE: Check == 2 -> enables the warm-up mode
+	    CHECK=$([ "$MATRIX_SIZE" == "2048" ] && echo 1 || echo 2)
+		NX_ARGS="--summary" timeout --preserve-status 150s ./build/cholesky-${EXEC_MODE} ${MATRIX_SIZE} ${CHECK}
+	    cat test_result.json >>$RES_FILE
+	    echo "," >>$RES_FILE
+	  done
+	done
+fi
